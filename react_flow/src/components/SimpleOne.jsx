02eb52d53407fd8e16503a1b1519ff1493b9d5c0
Vinit Gite <vinitcoding1001@gmail.com>
2024-12-27 08:05:43 +0530

    Intro to nodes and edges

--- react_flow/src/components/SimpleOne.jsx
+++ react_flow/src/components/SimpleOne.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
-import { Background, ReactFlow, Controls} from '@xyflow/react';
+import { Background, ReactFlow, Controls } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 const SimpleOne = () => {
+    const nodes = [
+        {
+            id: '1',
+            position: { x: 0, y: 0 },
+            data: { label: 'This is node 1' }
+        },
+        {
+            id: '2',
+            position: { x: 0, y: 100 },
+            data: { label: 'This is node 2' }
+        },
+        {
+            id: '3',
+            position: { x: 0, y: 200 },
+            data: { label: 'This is node 3' }
+        }
+    ]
+
+    const edges = [
+        {
+            id: '1-2',
+            source: '1',
+            target: '2',
+            label: 'To the step'
+        },
+        {
+            id: '2-3',
+            source: '2',
+            target: '3',
+            label: 'To the step'
+        }
+    ]
     return (
         <div className='h-full'>
-            <ReactFlow>
-                <Background />
+            <ReactFlow nodes={nodes} edges={edges}>
+                <Background variant='dots' />
                 <Controls />
             </ReactFlow>
         </div>
